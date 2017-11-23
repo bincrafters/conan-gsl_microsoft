6aad3902c9fff6c5d8246212e864b3c84fc0f683
John Shearer <shearer12345@gmail.com>
2017-11-23 09:52:48 +0000

    `import os` should be on its own line

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, os
+from conans import ConanFile
+import os
 
 
 class GslMicrosoftConan(ConanFile):
@@ -8,13 +9,12 @@ class GslMicrosoftConan(ConanFile):
     description = "Functions and types that are suggested for use by the C++ Core Guideline"
     license = "https://github.com/Microsoft/GSL/blob/master/LICENSE"
     source_url = "https://github.com/Microsoft/GSL"
-    
+
     def source(self):
         self.run("git clone --depth=1 {0}.git".format(self.source_url))
-        
+
     def package(self):
         self.copy(pattern="*", dst="include", src=os.path.join("GSL","include"))
 
     def package_id(self):
         self.info.header_only()
-
