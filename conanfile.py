141d50804c1a6a4e1b44bbfb29ea260e16e13a5f
Jerry W <jerrywiltse@gmail.com>
2018-01-02 14:43:08 -0500

    use specific commit

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ import os
 class GslMicrosoftConan(ConanFile):
     name = "gsl_microsoft"
     version = "20171020"
+    commit_id = "211e195d8f9dd6fa967a5741387fb5eae1ff351b"
     description = "Functions and types that are suggested for use by the C++ Core Guideline"
     url = "https://github.com/bincrafters/conan-gsl_microsoft"
     license = "MIT"
@@ -16,9 +17,11 @@ class GslMicrosoftConan(ConanFile):
     
     def source(self):
         source_url = "https://github.com/Microsoft/GSL"
-        self.run("git clone --depth=1 {0}.git".format(source_url))
+        self.run("git clone {0}.git".format(source_url))
         os.rename("GSL", self.source_subfolder)
-
+        with tools.chdir(self.source_subfolder):
+            self.run("git checkout -f {0}".format(self.commit_id))
+            
     def package(self):
         include_folder = os.path.join("source_subfolder","include")
         self.copy(pattern="LICENSE", src="source_subfolder")
